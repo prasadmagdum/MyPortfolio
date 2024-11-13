4a0fade643ad7d92d798238c40b3ed8612b959f0
Prasad Subhash Magdum <147171100+prasadmagdum@users.noreply.github.com>
2024-11-13 10:28:04 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import About from './components/About/About'
+import Activity from './components/Activity/Activity'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer.JSX'
+
+
+
+const App = () => {
+  return (
+    <div> 
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Activity/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
