a6b511f577f2143886c316bc92346c368da5cb46
Prasad Subhash Magdum <147171100+prasadmagdum@users.noreply.github.com>
2024-11-15 19:35:02 +0530

    Update Activity.jsx

--- components/Activity/Activity.jsx
+++ components/Activity/Activity.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import './Activity.css'
-import  image_EC from '../../assets/EC.JPG'
-import  image_PC from '../../assets/PC.JPG'
-import  image_AC from '../../assets/AC.JPG'
-
+import React from 'react';
+import './Activity.css';
+import image_EC from '../../assets/EC.JPG';
+import image_PC from '../../assets/PC.JPG';
+import image_AC from '../../assets/AC.JPG';
+import image_RC from '../../assets/RC.JPG'; 
 
 const activityImages = [
   { src: image_EC, alt: 'Extra Curricular Activity', caption: 'Extra Curricular' },
   { src: image_PC, alt: 'Professional Certificate', caption: 'Professional Certificate' },
-  { src: image_AC, alt: 'Certificate', caption: 'Certificate' }
+  { src: image_AC, alt: 'Academic Certificate', caption: 'Academic Certificate' }, 
+  { src: image_RC, alt: 'Research Certificate', caption: 'Research Certificate' } 
 ];
 
 const Activity = () => {
   return (
-    <div  id='activity'  className='activity'>
-      <div className='activity-title'>
+    <div id="activity" className="activity">
+      <div className="activity-title">
         <h1>My Activity</h1>
       </div>
-      <div className='activity-images'>
+      <div className="activity-images">
         {activityImages.map((image, index) => (
-          <div key={index} className='activity-image'>
+          <div key={index} className="activity-image">
             <img src={image.src} alt={image.alt} />
             <p>{image.caption}</p>
           </div>
